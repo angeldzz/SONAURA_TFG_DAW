bdcd069af37d697dc3b557fe43e155fe48e133c4
angeldzz <131559735+angeldzz@users.noreply.github.com>
2025-05-07 11:26:37 +0200

    adiccion noticias sonaura y mejoras en css

--- sonaura/proyecto_sonaura/base/urls.py
+++ sonaura/proyecto_sonaura/base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         path('peliculas',Peliculas.as_view(),name='peliculas'),#ruta a la pagina de peliculas
         path('series',Series.as_view(),name='series'),#ruta a la pagina de series
         path('detalles',Detalle_Pelicula_Serie.as_view(),name='detalles'),#ruta a la pagina de detalles
+        path('noticias',Noticias.as_view(),name='noticias'),#ruta a la pagina de noticias
 ]
--- sonaura/proyecto_sonaura/base/views.py
+++ sonaura/proyecto_sonaura/base/views.py
@@ -3,11 +3,14 @@ from django.views.generic import TemplateView,ListView,DetailView,CreateView,Del
 class Inicio(TemplateView):
     template_name = "base/inicio.html"
 
-class Detalle_Pelicula_Serie(TemplateView):
-    template_name = "base/detalle_pelicula_serie.html"
+class Noticias(TemplateView):
+    template_name = "base/noticias.html"
 
 class Peliculas(TemplateView):
     template_name = "base/peliculas.html"
 
 class Series(TemplateView):
     template_name = "base/series.html"
+
+class Detalle_Pelicula_Serie(TemplateView):
+    template_name = "base/detalle_pelicula_serie.html"
