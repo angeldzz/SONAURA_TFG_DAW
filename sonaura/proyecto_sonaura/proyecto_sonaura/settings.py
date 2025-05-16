872e1166bbcb532d06a5b2843ecfd62df5952708
angeldzz <131559735+angeldzz@users.noreply.github.com>
2025-05-16 10:48:16 +0200

    Login funciona Angel

--- sonaura/proyecto_sonaura/proyecto_sonaura/settings.py
+++ sonaura/proyecto_sonaura/proyecto_sonaura/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
 
 #imagenes
 MEDIA_URL = '/media/'
